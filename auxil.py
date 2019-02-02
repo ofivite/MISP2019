a72677cf77cb4bdac891494ae8134ab5b03cb740
Oleg Filatov <oleg.filatov@cern.ch>
2019-02-02 14:41:33 +0300

    added AMS metric from Giles

---
+++ auxil.py
@@ -0,0 +1,13 @@
+def AMS(s, b, br=10.0):
+    """ Approximate Median Significance defined as:
+        AMS = sqrt(
+                2 { (s + b + b_r) log[1 + (s/(b+b_r))] - s}
+              )        
+    where b_r = 10, b = background, s = signal, log is natural logarithm """
+    
+    radicand = 2 *( (s+b+br) * math.log (1.0 + s/(b+br)) -s)
+    if radicand < 0:
+        print('radicand is negative. Exiting')
+        return -1
+    else:
+return math.sqrt(radicand)
